c00d08cf4b9071b5448857169176d5cbb91c383f
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-27 11:10:53 +0200

    update

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -4,32 +4,32 @@
 
 char *get_next_line_bonus(int fd)
 {
-    int bytes;
-    char *line;
-    char buffer[BUFFER_SIZE + 1];
-    static char *stash[10240];
+	int bytes;
+	char *line;
+	char buffer[BUFFER_SIZE + 1];
+	static char *stash[10240];
 
-    if (fd < 0 || fd >= 10240 || BUFFER_SIZE <= 0)
-        return (NULL);
-    bytes = 1;
-    while (!ft_strchr(stash[fd], '\n') && bytes > 0)
-    {
-        bytes = read(fd, buffer, BUFFER_SIZE);
-        if (bytes < 0)
-        {
-            free(stash[fd]);
-            stash[fd] = NULL;
-            return (NULL);
-        }
-        buffer[bytes] = '\0';
-        stash[fd] = ft_strjoin(stash[fd], buffer);
-    }
-    line = extract_line(stash[fd]);
-    stash[fd] = update_stash(stash[fd]);
-    return (line);
+	if (fd < 0 || fd >= 10240 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(stash[fd], '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(stash[fd]);
+			stash[fd] = NULL;
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		stash[fd] = ft_strjoin(stash[fd], buffer);
+	}
+	line = extract_line(stash[fd]);
+	stash[fd] = update_stash(stash[fd]);
+	return (line);
 }
 
 char *get_next_line(int fd)
 {
-    return (get_next_line_bonus(fd));
+	return (get_next_line_bonus(fd));
 }
