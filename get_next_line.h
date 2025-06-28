5230279278ac71f05caeb406505b1a12082de7d4
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-28 09:00:22 +0200

    update

--- get_next_line.h
+++ get_next_line.h
@@ -1,20 +1,30 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 08:59:40 by esaleh            #+#    #+#             */
+/*   Updated: 2025/06/28 08:59:42 by esaleh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-char *get_next_line(int fd);
-size_t ft_strlen(const char *s);
-char *ft_strchr(const char *s, int c);
-char *ft_strjoin(char *s1, const char *s2);
-char *extract_line(char *stash);
-char *update_stash(char *stash);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, const char *s2);
+char	*extract_line(char *stash);
+char	*update_stash(char *stash);
 
 #endif
