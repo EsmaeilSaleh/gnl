6a6d94baeba3ddb7dcc24739915eb0582d10e7a2
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-28 08:49:24 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -1,24 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 08:49:16 by esaleh            #+#    #+#             */
+/*   Updated: 2025/06/28 08:49:24 by esaleh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
-    static char *stash;
-    char buffer[BUFFER_SIZE + 1];
-    char *line;
-    int bytes;
+	static char *stash;
+	char buffer[BUFFER_SIZE + 1];
+	char *line;
+	int bytes;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    bytes = 1;
-    while (!ft_strchr(stash, '\n') && bytes > 0)
-    {
-        bytes = read(fd, buffer, BUFFER_SIZE);
-        if (bytes < 0)
-            return (NULL);
-        buffer[bytes] = '\0';
-        stash = ft_strjoin(stash, buffer);
-    }
-    line = extract_line(stash);
-    stash = update_stash(stash);
-    return (line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(stash, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(stash); // clear static buffer on read error
+			stash = NULL;
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		stash = ft_strjoin(stash, buffer);
+	}
+	line = extract_line(stash);
+	stash = update_stash(stash);
+	return (line);
 }
